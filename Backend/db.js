204330a1b2fe0bc48fa3040f5e2ad72d10e0917e
Abhishek Kumar <72240889+Abhishek10269@users.noreply.github.com>
2024-10-14 21:52:20 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const todoSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  completed: { type: Boolean, default: false }
+  completed: { type: Boolean, default: true }
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
